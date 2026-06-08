33b3f9a13ad830261e14a8a95c6259e7c8d3ba8a
Straf LTI <pro.manuel.montoya@gmail.com>
2026-06-08 14:15:22 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,32 +3,41 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
-import { ThemeProvider } from "./contexts/ThemeContext";
+import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
-import Home from "./pages/Home";
+import { ThemeProvider } from "./contexts/ThemeContext";
 import BtsSio from "./pages/BtsSio";
-import Stages from "./pages/Stages";
-import Stage1 from "./pages/Stage1";
-import Stage2 from "./pages/Stage2";
-import TechWatch from "./pages/TechWatch";
 import Competences from "./pages/Competences";
 import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import LegalNotice from "./pages/LegalNotice";
+import Stage1 from "./pages/Stage1";
+import Stage2 from "./pages/Stage2";
+import Stages from "./pages/Stages";
+import TechWatch from "./pages/TechWatch";
 
 function Router() {
   return (
-    <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/bts-sio"} component={BtsSio} />
-      <Route path={"/stages"} component={Stages} />
-      <Route path={"/stage/1"} component={Stage1} />
-      <Route path={"/stage/2"} component={Stage2} />
-      <Route path={"/veille"} component={TechWatch} />
-      <Route path={"/competences"} component={Competences} />
-      <Route path={"/contact"} component={Contact} />
-      <Route path={"/404"} component={NotFound} />
-      {/* Final fallback route */}
-      <Route component={NotFound} />
-    </Switch>
+    <div className="flex flex-col min-h-screen">
+      <Navigation />
+      <main className="flex-1">
+        <Switch>
+          <Route path={"/"} component={Home} />
+          <Route path={"/bts-sio"} component={BtsSio} />
+          <Route path={"/stages"} component={Stages} />
+          <Route path={"/stage1"} component={Stage1} />
+          <Route path={"/stage2"} component={Stage2} />
+          <Route path={"/veille"} component={TechWatch} />
+          <Route path={"/competences"} component={Competences} />
+          <Route path={"/contact"} component={Contact} />
+          <Route path={"/mentions-legales"} component={LegalNotice} />
+          <Route path={"/404"} component={NotFound} />
+          {/* Final fallback route */}
+          <Route component={NotFound} />
+        </Switch>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
@@ -38,7 +47,6 @@ function App() {
       <ThemeProvider defaultTheme="light" switchable>
         <TooltipProvider>
           <Toaster />
-          <Navigation />
           <Router />
         </TooltipProvider>
       </ThemeProvider>
